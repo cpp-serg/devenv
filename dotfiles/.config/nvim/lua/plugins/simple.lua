03ff694fe8c9aa6e189bc90e97bb8d4237c0cc8b
Sergiy Pastukhov <sergiy@jpu.io>
2024-03-14 18:59:24 +0200

    Add a few more plugins

--- dotfiles/.config/nvim/lua/plugins/simple.lua
+++ dotfiles/.config/nvim/lua/plugins/simple.lua
@@ -1,8 +1,10 @@
 return {
-    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-    'nvim-lua/plenary.nvim', -- Utilities widely used in other plugins
+    { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
+    { 'nvim-lua/plenary.nvim' }, -- Utilities widely used in other plugins
     { 'folke/neoconf.nvim', cmd = 'Neoconf' },
-    'folke/neodev.nvim',
-    'tpope/vim-fugitive',
-    'rhysd/vim-clang-format',
+    { 'folke/neodev.nvim'},
+    { 'tpope/vim-fugitive'},
+    { 'rhysd/vim-clang-format'},
+    { 'numToStr/Comment.nvim', opts = {} },
+    { 'folke/trouble.nvim' }
 }
