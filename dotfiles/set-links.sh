0a8cf833a65a9a0bcdc407571e39c285c6deabda
Sergiy Pastukhov <sergiy@jpu.io>
2024-07-29 22:16:52 +0300

    Back up older configs

--- dotfiles/set-links.sh
+++ dotfiles/set-links.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# check if names below exist, rename them to bak
+if [ -e ~/.config] ; then mv ~/.config ~/.config.bak; fi
+if [ -e ~/.tmux] ; then mv ~/.tmux ~/.tmux.bak; fi
+if [ -e ~/.zshrc] ; then mv ~/.zshrc ~/.zshrc.bak; fi
+
 ln -s ~/devenv/dotfiles/.config ~/.config
 ln -s ~/devenv/dotfiles/.tmux ~/.tmux
 ln -s ~/devenv/dotfiles/.zshrc ~/.zshrc
