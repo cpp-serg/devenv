8c5f98f84230bf7daf440e161b2464cb9a67a068
Sergiy Pastukhov <sergiy@jpu.io>
2024-08-22 14:55:54 +0300

    add mini.icons

--- dotfiles/.config/nvim/lua/plugins/simple.lua
+++ dotfiles/.config/nvim/lua/plugins/simple.lua
@@ -1,10 +1,17 @@
 return {
-    { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
     { 'nvim-lua/plenary.nvim' }, -- Utilities widely used in other plugins
+
+    -- icons
+    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    { 'echasnovski/mini.icons', version = false },
+
+    { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
+    { 'tpope/vim-fugitive'},
+
     { 'folke/neoconf.nvim', cmd = 'Neoconf' },
     { 'folke/neodev.nvim'},
-    { 'tpope/vim-fugitive'},
+    { 'folke/trouble.nvim' },
+
     { 'rhysd/vim-clang-format'},
     { 'numToStr/Comment.nvim', opts = {} },
-    { 'folke/trouble.nvim' }
 }
