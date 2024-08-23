9671deacee546515b79b96191390b43788f15b2a
Sergiy Pastukhov <sergiy@jpu.io>
2024-08-23 12:33:40 +0300

    Update supermaven color

--- dotfiles/.config/nvim/lua/plugins/supermaven.lua
+++ dotfiles/.config/nvim/lua/plugins/supermaven.lua
@@ -1,6 +1,12 @@
 return {
   "supermaven-inc/supermaven-nvim",
   config = function()
-    require("supermaven-nvim").setup({})
+    require("supermaven-nvim").setup({
+
+      color = {
+        suggestion_color = "#afffff",
+        cterm = 159,
+      },
+    })
   end,
 }
