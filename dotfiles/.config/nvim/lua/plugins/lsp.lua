6ebb2d70e6ff8dcfcd5278991b38cad4adb18097
Sergiy Pastukhov <sergiy@jpu.io>
2024-08-23 23:40:10 +0300

    Cleanup lsp

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,5 @@
 function ToggleInlineHints()
-    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
 end
 
 return {
@@ -12,95 +12,40 @@ return {
             'WhoIsSethDaniel/mason-tool-installer.nvim',
 
             -- Useful status updates for LSP.
-            -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
             { 'j-hui/fidget.nvim', opts = {} },
         },
         config = function()
-            -- Brief Aside: **What is LSP?**
-            --
-            -- LSP is an acronym you've probably heard, but might not understand what it is.
-            --
-            -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-            -- and language tooling communicate in a standardized fashion.
-            --
-            -- In general, you have a "server" which is some tool built to understand a particular
-            -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc). These Language Servers
-            -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-            -- processes that communicate with some "client" - in this case, Neovim!
-            --
-            -- LSP provides Neovim with features like:
-            --  - Go to definition
-            --  - Find references
-            --  - Autocompletion
-            --  - Symbol Search
-            --  - and more!
-            --
-            -- Thus, Language Servers are external tools that must be installed separately from
-            -- Neovim. This is where `mason` and related plugins come into play.
-            --
-            -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-            -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
-            --  This function gets run when an LSP attaches to a particular buffer.
-            --    That is to say, every time a new file is opened that is associated with
-            --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-            --    function will be executed to configure the current buffer
             vim.api.nvim_create_autocmd('LspAttach', {
                 group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
                 callback = function(event)
-                    -- NOTE: Remember that lua is a real programming language, and as such it is possible
-                    -- to define small helper and utility functions so you don't have to repeat yourself
-                    -- many times.
-                    --
-                    -- In this case, we create a function that lets us more easily define mappings specific
-                    -- for LSP related items. It sets the mode, buffer and description for us each time.
                     local map = function(keys, func, desc)
                         vim.keymap.set('n', keys, func, {
                             buffer = event.buf,
                             desc = 'LSP: ' .. desc,
                         })
                     end
+                    local ts_bi = require('telescope.builtin')
 
-                    -- Jump to the definition of the word under your cursor.
-                    --  This is where a variable was first declared, or where a function is defined, etc.
-                    --  To jump back, press <C-T>.
-                    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+                    map('gd', ts_bi.lsp_definitions, '[G]oto [D]efinition')
+                    map('gI', ts_bi.lsp_implementations, '[G]oto [I]mplementation')
 
-                    -- Find references for the word under your cursor.
-                    -- map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-                    -- Jump to the implementation of the word under your cursor.
-                    --  Useful when your language has ways of declaring types without an actual implementation.
-                    map(
-                        'gI',
-                        require('telescope.builtin').lsp_implementations,
-                        '[G]oto [I]mplementation'
-                    )
 
                     -- Jump to the type of the word under your cursor.
                     --  Useful when you're not sure what type a variable is and you want to see
                     --  the definition of its *type*, not where it was *defined*.
-                    map(
-                        '<leader>D',
-                        require('telescope.builtin').lsp_type_definitions,
-                        'Type [D]efinition'
-                    )
+                    map('gt', ts_bi.lsp_type_definitions, 'Type [D]efinition')
 
                     -- Fuzzy find all the symbols in your current document.
                     --  Symbols are things like variables, functions, types, etc.
-                    map(
-                        '<leader>ds',
-                        require('telescope.builtin').lsp_document_symbols,
-                        '[D]ocument [S]ymbols'
-                    )
+                    map('<leader>ds', ts_bi.lsp_document_symbols, '[D]ocument [S]ymbols')
 
                     -- Fuzzy find all the symbols in your current workspace
                     --  Similar to document symbols, except searches over your whole project.
-                    map(
-                        '<leader>ws',
-                        require('telescope.builtin').lsp_dynamic_workspace_symbols,
-                        '[W]orkspace [S]ymbols'
-                    )
+                    map('<leader>ws', ts_bi.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                    -- map('<leader>ws', ts_bi.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
+
+                    map('<leader>ic', ts_bi.lsp_incoming_calls, '[W]orkspace [S]ymbols')
+                    map('<leader>oc', ts_bi.lsp_outgoing_calls, '[W]orkspace [S]ymbols')
 
                     -- Rename the variable under your cursor
                     --  Most Language Servers support renaming across files, etc.
@@ -272,11 +217,5 @@ return {
                 },
             })
         end,
-    },
-    {
-        'neovim/nvim-lspconfig',
-        opts = {
-            inlay_hints = { enabled = true },
-        },
     },
 }
