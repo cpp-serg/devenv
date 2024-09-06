825cda7c1fcc996acfbd288075952f974503d614
Sergiy Pastukhov <sergiy@jpu.io>
2024-09-07 02:50:01 +0300

    Update set-links

--- dotfiles/set-links.sh
+++ dotfiles/set-links.sh
@@ -2,10 +2,8 @@
 
 # check if names below exist, rename them to bak
 [[ -e ~/.config ]] && mv ~/.config ~/.config.bak
-[[ -e ~/.tmux ]] && mv ~/.tmux ~/.tmux.bak
 [[ -e ~/.zshrc ]] && mv ~/.zshrc ~/.zshrc.bak
 
 ln -s ~/devenv/dotfiles/.config ~/.config
-ln -s ~/devenv/dotfiles/.tmux ~/.tmux
 ln -s ~/devenv/dotfiles/.zshrc ~/.zshrc
 
