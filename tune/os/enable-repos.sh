0d793cd3ff7e8e3512d04b6b68be86f50883a315
Sergiy Pastukhov <sergiy@jpu.io>
2024-09-08 02:50:59 +0300

    Add crb

--- tune/os/enable-repos.sh
+++ tune/os/enable-repos.sh
@@ -8,6 +8,7 @@ if (yum repolist all appstream | grep disabled >/dev/null) then
     ${SUDO} dnf config-manager --enable "*"
     ${SUDO} dnf config-manager --disable "media*"
     ${SUDO} dnf install -y epel-release
+    ${SUDO} /usr/bin/crb enable
 else
     echo "Repos are enabled"
 fi
