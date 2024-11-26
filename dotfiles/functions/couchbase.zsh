129f9160117b4c597501ca2a5606d590f2cc805c
Sergiy Pastukhov <sergiy@jpu.io>
2024-11-26 02:21:35 +0000

    Update couchbase.zsh

--- dotfiles/functions/couchbase.zsh
+++ dotfiles/functions/couchbase.zsh
@@ -1,92 +1,126 @@
 USER=Administrator
 PASS=admin123
+
 HOST=127.0.0.1
+BUCKET=JPUHLR
+
+typeset -A __cb_PGW_IDS # associative array logical name -> regex
+__cb_PGW_IDS=(
+    main      "[0-9]+::.*[.]json"
+    prim      "jpu::Primary::[0-9]+"
+    secondry  "jpu::Secondry::[0-9]+"
+    usedbytes "jpu::[^:]+UsedBytes::[0-9]+"
+    eu_ip     "jpu::tun[0-9]+::[0-9.]+"
+    remote    "jpu::[0-9.]+:[0-9]+"
+)
+
+# name is key of __cb_PGW_IDS
+function __cb_getRegex {
+    name=$1
+    echo "${__cb_PGW_IDS[$name]}"
+}
+
+function __cb_makeMatch {
+    name=$1
+    echo "regexp_matches(meta().id, '$(__cb_getRegex $name)')"
+}
+
+function __cb_makeAllPgwIdsMatch {
+    matches=()
+    for name in ${(k)__cb_PGW_IDS}; do
+        matches+="$(__cb_makeMatch ${name})"
+    done
+    echo "${(j: or :)matches}" # join with ' or ' as a separator
+}
+
+__cb_PGW_ID_MATCH=$(__cb_makeAllPgwIdsMatch)
 
 function cb-q
 {
+    if [[ "$SHOW_QUERY" == "1" ]]; then
+        echo "$*;" | sed -E  -e "s/(where| or|COUNT|from)/\n  \1/g" >&2
+        exit 0
+    fi
+
     #Get first arg of exit with error
     cbq -q -u $USER -p $PASS -script "$*;" $HOST
 }
 
 function cb-all-keys
 {
-    BUCKET=${1:-JPUHLR}
-    cb-q "select meta().id from ${BUCKET};" | jq -r ".results[].id"
+    cb-q "select raw meta().id from ${BUCKET}" \
+    | jq -r ".results[]"
 }
 
-function cb-ls-re-keys {
-    cb-q "select meta().id            from JPUHLR where regexp_matches(meta().id, '$1')" | jq -r ".results[].id"
+function cb-ls-keys {
+    cb-q "select raw meta().id from JPUHLR where regexp_matches(meta().id, '$1')" \
+    | jq -r ".results[]"
 }
 
-function cb-count-re-keys {
-    cb-q "select raw count(meta().id) from JPUHLR where regexp_matches(meta().id, '$1')" | jq -r ".results[]"
+function cb-count-keys {
+    cb-q "select raw count(meta().id) from JPUHLR where regexp_matches(meta().id, '$1')" \
+    | jq -r ".results[]"
 }
 
-function cb-rm-re-keys {
-    cb-q "delete                      from JPUHLR where regexp_matches(meta().id, '$1')" | jq -r ".metrics.mutationCount"
+function cb-rm-keys {
+    cb-q "delete from JPUHLR where regexp_matches(meta().id, '$1')" \
+    | jq -r ".metrics.mutationCount"
 }
-
-function cb-ls-reneg-keys {
-    cb-q "select meta().id            from JPUHLR where not regexp_matches(meta().id, '$1')" | jq -r ".results[].id"
-}
-
-function cb-count-reneg-keys {
-    cb-q "select raw count(meta().id) from JPUHLR where not regexp_matches(meta().id, '$1')" | jq -r ".results[]"
-}
-
-function cb-rm-reneg-keys {
-    cb-q "delete                      from JPUHLR where not regexp_matches(meta().id, '$1')" | jq -r ".metrics.mutationCount"
-}
-
-
-function cb-pgw-keys {
-    cb-ls-reneg-keys "^(user::|auc::|IMSI_|sys_conf_|opname::|keys::|k4name::|AUC_JPU_KEYSET|iccid_imsi::|securedSuciKey::).*"
-}
-
 
 function cb-pgw-main-keys {
-    # cb-ls-re-keys "[0-9]+::.*\.json"
-    cb-ls-re-keys "[0-9]+::.*[.]json"
-    # cb-ls-re-keys ".json"
+    cb-ls-keys "$(__cb_getRegex main)"
 }
 
 function cb-pgw-prim-keys {
-    cb-ls-re-keys "jpu::Primary::[0-9]+"
+    cb-ls-keys "$(__cb_getRegex prim)"
 }
 
 function cb-pgw-sec-keys {
-    cb-ls-re-keys "jpu::Secondry::[0-9]+"
+    cb-ls-keys "$(__cb_getRegex secondry)"
 }
 
 function cb-pgw-ub-keys {
-    cb-ls-re-keys "jpu::[^:]+UsedBytes::[0-9]+"
+    cb-ls-keys "$(__cb_getRegex usedbytes)"
 }
 
-function cb-pgw-tun-keys {
-    cb-ls-re-keys "jpu::tun[0-9]+::[0-9.]+"
+function cb-pgw-eu_ip-keys {
+    cb-ls-keys "$(__cb_getRegex eu_ip)"
 }
 
 function cb-pgw-remote-keys {
-    cb-ls-re-keys "jpu::[0-9.]+:[0-9]+"
+    cb-ls-keys "$(__cb_getRegex remote)"
+}
+
+function cb-show-session {
+    imsi=$1
+    cb-q "select * from JPUHLR where regexp_matches(meta().id, '${imsi}::.*.json')" \
+    | jq -r --color-output ".results[].JPUHLR.pdp| del(.sessionQos,.nssai,.pcoNeg,.pcoReq, .secPdp[].qos, .userLocationInfo, .upLocation)"
+}
+
+function cb-pgw-counts {
+    matches=()
+    for name in ${(k)__cb_PGW_IDS}; do
+        matches+="COUNT( case when $(__cb_makeMatch ${name}) THEN 1 END) as ${name}"
+    done
+
+    cb-q "select ${(j:, :)matches} from JPUHLR" \
+    | jq -r -M '.results[] | to_entries | .[] | "\(.key) \(.value)"' \
+    | column -t
+}
+
+function cb-pgw-keys {
+    cb-q "select raw meta().id from JPUHLR where ${__cb_PGW_ID_MATCH}" \
+    | jq -r '.results[]'
+}
+
+function cb-pgw-exps {
+cb-q "select meta().id,  DATE_ADD_STR('1970-01-01T00:00:00Z', meta().expiration, 'second') AS exp_dt from JPUHLR where ${__cb_PGW_ID_MATCH}" \
+    | jq -r -M '.results[] | "\(.exp_dt) \(.id)"' 
 }
 
 
-function cb-count-pgw-keys {
-    echo -n "Main: "
-    cb-pgw-main-keys | wc -l
-    echo -n "Primary: "
-    cb-pgw-prim-keys | wc -l
-    echo -n "Secondary: "
-    cb-pgw-sec-keys | wc -l
-    echo -n "Remote: "
-    cb-pgw-remote-keys | wc -l
-    echo -n "Tunnels: "
-    cb-pgw-tun-keys | wc -l
-    echo -n "Used Bytes: "
-    cb-pgw-ub-keys | wc -l
+function cb-rm-pgw-keys {
+    cb-q "delete from JPUHLR where ${__cb_PGW_ID_MATCH}" \
+    | jq -r ".metrics.mutationCount"
 }
 
-
-# function cb-pgw-keys {
-#     cb-all-keys | rg -v  "^(user::|auc::|IMSI_|sys_conf_|opname::|keys::|k4name::|AUC_JPU_KEYSET|iccid_imsi::)"
-# }
