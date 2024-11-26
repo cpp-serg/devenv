400e314a257023aadf9248c82454e08159737546
Sergiy Pastukhov <sergiy@jpu.io>
2024-11-26 02:21:35 +0000

    Update couchbase.zsh

--- dotfiles/functions/couchbase.zsh
+++ dotfiles/functions/couchbase.zsh
@@ -38,16 +38,8 @@ function cb-rm-reneg-keys {
     cb-q "delete                      from JPUHLR where not regexp_matches(meta().id, '$1')" | jq -r ".metrics.mutationCount"
 }
 
-
-function cb-pgw-keys {
-    cb-ls-reneg-keys "^(user::|auc::|IMSI_|sys_conf_|opname::|keys::|k4name::|AUC_JPU_KEYSET|iccid_imsi::|securedSuciKey::).*"
-}
-
-
 function cb-pgw-main-keys {
-    # cb-ls-re-keys "[0-9]+::.*\.json"
     cb-ls-re-keys "[0-9]+::.*[.]json"
-    # cb-ls-re-keys ".json"
 }
 
 function cb-pgw-prim-keys {
@@ -70,23 +62,48 @@ function cb-pgw-remote-keys {
     cb-ls-re-keys "jpu::[0-9.]+:[0-9]+"
 }
 
-
-function cb-count-pgw-keys {
-    echo -n "Main: "
-    cb-pgw-main-keys | wc -l
-    echo -n "Primary: "
-    cb-pgw-prim-keys | wc -l
-    echo -n "Secondary: "
-    cb-pgw-sec-keys | wc -l
-    echo -n "Remote: "
-    cb-pgw-remote-keys | wc -l
-    echo -n "Tunnels: "
-    cb-pgw-tun-keys | wc -l
-    echo -n "Used Bytes: "
-    cb-pgw-ub-keys | wc -l
+function cb-show-dev
+{
+    imsi=$1
+    cb-q "select * from JPUHLR where regexp_matches(meta().id, '${imsi}::.*.json')" \
+    | jq -r --color-output ".results[].JPUHLR.pdp| del(.sessionQos,.nssai,.pcoNeg,.pcoReq, .secPdp[].qos, .userLocationInfo, .upLocation)"
 }
 
+function cb-show-pgw-counts
+{
+    imsi=$1
+    cb-q "select COUNT(case when regexp_matches(meta().id, '[0-9]+::.*[.]json') THEN 1 END) as main,\
+        COUNT(case when regexp_matches(meta().id, 'jpu::Primary::[0-9]+') THEN 1 END) as prim,\
+        COUNT(case when regexp_matches(meta().id, 'jpu::Secondry::[0-9]+') THEN 1 END) as secondary,\
+        COUNT(case when regexp_matches(meta().id, 'jpu::tun[0-9]+::[0-9.]+') THEN 1 END) as tunnel,\
+        COUNT(case when regexp_matches(meta().id, 'jpu::[0-9.]+:[0-9]+') THEN 1 END) as remote,\
+        COUNT(case when regexp_matches(meta().id, 'jpu::[^:]+UsedBytes::[0-9]+') THEN 1 END) as usedbytes from JPUHLR"\
+    | jq -r -M '.results[] | to_entries | .[] | "\(.key) \(.value)"'\
+    | column -t
+}
 
-# function cb-pgw-keys {
-#     cb-all-keys | rg -v  "^(user::|auc::|IMSI_|sys_conf_|opname::|keys::|k4name::|AUC_JPU_KEYSET|iccid_imsi::)"
-# }
+function cb-pgw-keys
+{
+    cb-q "select meta().id from JPUHLR where \
+        regexp_matches(meta().id, '[0-9]+::.*[.]json') \
+        or regexp_matches(meta().id, 'jpu::Primary::[0-9]+') \
+        or regexp_matches(meta().id, 'jpu::Secondry::[0-9]+') \
+        or regexp_matches(meta().id, 'jpu::tun[0-9]+::[0-9.]+') \
+        or regexp_matches(meta().id, 'jpu::[0-9.]+:[0-9]+') \
+        or regexp_matches(meta().id, 'jpu::[^:]+UsedBytes::[0-9]+');" \
+    | jq -r -M '.results[].id'
+}
+
+function cb-rm-pgw-keys
+{
+    cb-q "delete from JPUHLR where \
+        regexp_matches(meta().id, '[0-9]+::.*[.]json') \
+        or regexp_matches(meta().id, 'jpu::Primary::[0-9]+') \
+        or regexp_matches(meta().id, 'jpu::Secondry::[0-9]+') \
+        or regexp_matches(meta().id, 'jpu::tun[0-9]+::[0-9.]+') \
+        or regexp_matches(meta().id, 'jpu::[0-9.]+:[0-9]+') \
+        or regexp_matches(meta().id, 'jpu::[^:]+UsedBytes::[0-9]+');" \
+        | jq -r ".metrics.mutationCount";
+
+}
+
