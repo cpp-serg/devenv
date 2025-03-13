1ebf59e7178bdd751f6b5bb922c887b5ade50db3
Sergiy Pastukhov <sergiy@jpu.io>
2025-03-14 01:30:39 +0200

    Add git-lfs

--- tune/os/install-soft.sh
+++ tune/os/install-soft.sh
@@ -6,8 +6,8 @@ echo ${INSTALL_DIR}
 # set ${SUDO} conditionally
 SUDO=$([ $(id -u) -ne 0 ] && echo sudo)
 
-${SUDO} dnf install -y util-linux-user tar bzip2 unzip python3 sqlite htop ripgrep fd-find ninja-build make dos2unix lbzip2 cmake gcc-c++
-${INSTALL_DIR}/install-git.sh
+${SUDO} dnf install -y util-linux-user git git-lfs tar bzip2 unzip python3 sqlite htop ripgrep fd-find ninja-build make dos2unix lbzip2 cmake gcc-c++
+# ${INSTALL_DIR}/install-git.sh
 ${INSTALL_DIR}/install-cmake.sh
 ${INSTALL_DIR}/install-tmux.sh
 ${INSTALL_DIR}/install-neovim.sh
