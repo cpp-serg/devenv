357304ae7336c55f7cdfdeed6d8804a1d200e6fe
Sergiy Pastukhov <sergiy@jpu.io>
2025-03-31 03:06:57 +0300

    Fix ip for jenkins, restart sshd

--- tune/os/configure-services.sh
+++ tune/os/configure-services.sh
@@ -8,6 +8,7 @@ ${SUDO} sed -i 's/^Banner /#Banner /' /etc/ssh/sshd_config
 
 # enable ssh forwarding for remove VSCode/Cursor
 ${SUDO} sed -i -E 's/^(AllowTcpForwarding.*)no/\1yes/g' /etc/ssh/sshd_config
+${SUDO} systemctl restart sshd
 
 # disable tmux lock
 ${SUDO} sed -i 's/^set -g lock/#set -g lock/g' /etc/tmux.conf
@@ -16,7 +17,7 @@ ${SUDO} sed -i 's/^set -g lock/#set -g lock/g' /etc/tmux.conf
 # Add fstab entry for builds
 if ! grep -q "/Builds" /etc/fstab; then
     echo "Adding fstab entry for builds"
-    ${SUDO} echo '//12.20.7.41/Builds /mnt/builds cifs username=sergiy@pentenetworks.com  0 0' >> /etc/fstab
+    ${SUDO} echo '//10.20.7.41/Builds /mnt/builds cifs username=sergiy@pentenetworks.com  0 0' >> /etc/fstab
     ${SUDO} systemctl daemon-reload
 fi
 
