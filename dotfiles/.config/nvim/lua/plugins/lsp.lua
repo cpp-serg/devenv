fb08419b400c6ebd20621faf79ef33e566b78041
Sergiy Pastukhov <sergiy@jpu.io>
2025-07-21 20:49:20 +0300

    Fix lua LST config

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -161,14 +161,17 @@ return {
                     -- capabilities = {},
                     settings = {
                         Lua = {
-                            runtime = { version = 'LuaJIT' },
+                            runtime = {
+                                version = 'LuaJIT',
+                            },
                             workspace = {
                                 checkThirdParty = false,
                                 -- Tells lua_ls where to find all the Lua files that you have loaded
                                 -- for your neovim configuration.
                                 library = {
                                     '${3rd}/luv/library',
-                                    unpack(vim.api.nvim_get_runtime_file('', true)),
+                                    vim.api.nvim_get_runtime_file('', true),
+                                    checkThirdParty = true,
                                 },
                                 -- If lua_ls is really slow on your computer, you can try this instead:
                                 -- library = { vim.env.VIMRUNTIME },
@@ -178,6 +181,9 @@ return {
                             },
                             -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
                             -- diagnostics = { disable = { 'missing-fields' } },
+                            diagnostics = {
+                                globals = { 'vim' },
+                            },
                         },
                     },
                 },
