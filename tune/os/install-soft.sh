3556e8b4d171549f4506d059f65f04f6b35ce9a5
Sergiy Pastukhov <sergiy@jpu.io>
2026-02-23 13:24:19 +0200

    Add findutils to installed packages

--- tune/os/install-soft.sh
+++ tune/os/install-soft.sh
@@ -6,7 +6,7 @@ echo ${INSTALL_DIR}
 # set ${SUDO} conditionally
 SUDO=$([ $(id -u) -ne 0 ] && echo sudo)
 
-${SUDO} dnf install -y util-linux-user git git-lfs tar bzip2 unzip python3 sqlite htop ripgrep fd-find ninja-build make dos2unix lbzip2 cmake gcc-c++
+${SUDO} dnf install -y util-linux-user findutils git git-lfs tar bzip2 unzip python3 sqlite htop ripgrep fd-find ninja-build make dos2unix lbzip2 cmake gcc-c++
 # ${INSTALL_DIR}/install-git.sh
 ${INSTALL_DIR}/install-cmake.sh
 ${INSTALL_DIR}/install-tmux.sh
