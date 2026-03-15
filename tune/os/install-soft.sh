cd1dc3ba826f9595c9cd9266ca44c77d92f78a6f
Sergiy Pastukhov <sergiy@jpu.io>
2026-03-15 18:32:56 +0200

    disable cmake installation from sources

--- tune/os/install-soft.sh
+++ tune/os/install-soft.sh
@@ -8,7 +8,7 @@ SUDO=$([ $(id -u) -ne 0 ] && echo sudo)
 
 ${SUDO} dnf install -y util-linux-user findutils git git-lfs tar bzip2 unzip python3 sqlite htop ripgrep fd-find ninja-build make dos2unix lbzip2 cmake gcc-c++
 # ${INSTALL_DIR}/install-git.sh
-${INSTALL_DIR}/install-cmake.sh
+# ${INSTALL_DIR}/install-cmake.sh
 ${INSTALL_DIR}/install-tmux.sh
 ${INSTALL_DIR}/install-neovim.sh
 ${MY_DIR}/dotfiles/fzf/install --bin
