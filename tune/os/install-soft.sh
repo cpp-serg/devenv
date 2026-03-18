7880884574bf7d3c1d42a9728e7bfd90101706d6
Sergiy Pastukhov <sergiy@jpu.io>
2026-03-18 12:13:22 +0200

    add ncdu

--- tune/os/install-soft.sh
+++ tune/os/install-soft.sh
@@ -3,10 +3,9 @@
 MY_DIR=$(cd $(dirname $0); pwd)
 INSTALL_DIR=$(cd ${MY_DIR}/../../install; pwd)
 echo ${INSTALL_DIR}
-# set ${SUDO} conditionally
 SUDO=$([ $(id -u) -ne 0 ] && echo sudo)
 
-${SUDO} dnf install -y util-linux-user findutils git git-lfs tar bzip2 unzip python3 sqlite htop ripgrep fd-find ninja-build make dos2unix lbzip2 cmake gcc-c++
+${SUDO} dnf install -y util-linux-user findutils git git-lfs tar bzip2 unzip python3 sqlite htop ncdu ripgrep fd-find ninja-build make dos2unix lbzip2 cmake gcc-c++
 # ${INSTALL_DIR}/install-git.sh
 # ${INSTALL_DIR}/install-cmake.sh
 ${INSTALL_DIR}/install-tmux.sh
